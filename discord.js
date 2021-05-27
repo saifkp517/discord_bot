63dc59d5a9d0ef48941c5e490cfc610b9d575bed
Saif5017 <saifullahkhan@cmr.ac.in>
2021-05-27 23:11:09 +0530

    fixed http requests

--- discord.js
+++ discord.js
@@ -70,6 +70,11 @@ client.on('message', message => {
     message.channel.send('thanks')
   }
 
+  let cat = 3
+  if(cat == 3){
+    message.channel.send('cat')
+  }
+
   if(message.content === '!tweet'){
     fetch('https://twitter.com/i/api/graphql/Kq7XqqyDGn4Ly7Yh0AhK9w/UserTweetsAndReplies?variables=%7B%22userId%22%3A%2244196397%22%2C%22count%22%3A20%2C%22withHighlightedLabel%22%3Atrue%2C%22withTweetQuoteCount%22%3Atrue%2C%22includePromotedContent%22%3Atrue%2C%22withTweetResult%22%3Afalse%2C%22withReactions%22%3Afalse%2C%22withUserResults%22%3Afalse%2C%22withVoice%22%3Afalse%2C%22withNonLegacyCard%22%3Atrue%2C%22withBirdwatchPivots%22%3Afalse%7D', {
       headers: {
